c4ef340b856b90b92fda418e551acd915e468638
3am-solver <work24to7@gmail.com>
2022-12-21 02:46:03 +0530

    updated data classes

--- app/src/main/java/com/example/nmsadminapp/models/ReviewModel.kt
+++ app/src/main/java/com/example/nmsadminapp/models/ReviewModel.kt
@@ -1,4 +1,15 @@
 package com.example.nmsadminapp.models
 
-class ReviewModel {
-}
+data class ReviewModel(
+    val reviewId: String,
+    val reviewProductId: String,
+    val reviewUserId: String,
+    val reviewUserName: String,
+    val reviewUserImage: String,
+    val reviewRating: String,
+    val reviewTitle: String,
+    val reviewDescription: String,
+    val reviewDate: String,
+    val reviewTime: String,
+    val reviewStatus: String
+)
--- app/src/main/java/com/example/nmsadminapp/models/UsersModel.kt
+++ app/src/main/java/com/example/nmsadminapp/models/UsersModel.kt
@@ -1,4 +1,20 @@
 package com.example.nmsadminapp.models
 
-class UsersModel {
-}
+data class UsersModel(
+    val userId: String,
+    val userName: String,
+    val userMobile: String,
+    val userEmail: String,
+    val userImage: String,
+    val userAddress: String,
+    val userPincode: String,
+    val userCity: String,
+    val userState: String,
+    val userCountry: String,
+    val userLandmark: String,
+    val userAlternateMobile: String,
+    val userAddressType: String,
+    val userAddressStatus: String,
+    val userAddressAddedOn: String,
+    val userAddressUpdatedOn: String
+)
